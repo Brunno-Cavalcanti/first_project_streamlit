8221611f7139960f9dc04d0e874c6a64473e7110
Bruno Cavalcanti <bruno.cubo2011@gmail.com>
2025-08-12 13:19:34 -0300

    Atualizar app para usar variáveis de ambiente e requirements

--- app.py
+++ app.py
@@ -1,4 +1,25 @@
+import os
 import streamlit as st
+import pandas as pd
+import pyodbc
 
-st.title("Teste básico Streamlit")
-st.write("Se isso aparecer, a configuração está correta.")
+server = os.getenv('DB_SERVER')
+database = os.getenv('DB_NAME')
+username = os.getenv('DB_USER')
+password = os.getenv('DB_PASS')
+driver = '{ODBC Driver 17 for SQL Server}'
+
+conn_str = (
+    f'DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}'
+)
+
+try:
+    conn = pyodbc.connect(conn_str)
+    st.success("Conectado ao banco SQL Server!")
+except Exception as e:
+    st.error(f"Erro na conexão: {e}")
+    st.stop()
+
+query = "SELECT TOP 10 * FROM negociacao"
+df = pd.read_sql(query, conn)
+st.dataframe(df)
